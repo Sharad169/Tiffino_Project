bb945a98c0e57d26f6b4f99cd23b38f4e084a911
Sharad169 <sharadmohite7020@gmail.com>
2025-08-31 19:07:31 +0530

    create register form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tiffino';
+
+
+  showLogin = true;   // pehle Login form dikhega
+  showRegister = false;
+
+  openLogin() {
+    this.showLogin = true;
+    this.showRegister = false;
+  }
+
+  openRegister() {
+    this.showRegister = true;
+    this.showLogin = false;
+  }
 }
